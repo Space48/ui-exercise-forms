c46708b076639ee938cd77af9100746fd5a02104
Rui Correia <rmgc31@gmail.com>
2020-07-04 10:42:10 +0100

    setup the environment to run the component in react. deleted all the parcel dependencies

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div>This will be the form.</div>;
+  }
+}
+
+export default App;
