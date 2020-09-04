a71a3130ce21e3496ac26d00be97be2bbe560b67
Razi Syed <candy.razi@gmail.com>
2020-09-04 10:12:24 +0100

    one hour work on this exc

---
+++ src/scripts/app.ts
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+// axios({
+//     method: 'post',
+//     url: 'http://localhost:3005/customer/account/resetPassword',
+//     data: '' ,
+//     headers: {'Content-Type': 'multipart/form-data' }
+//     })
+//     .then(function (response) {
+//         console.log(response);
+//     })
+//     .catch(function (response) {
+//         console.log(response);
+//     });
