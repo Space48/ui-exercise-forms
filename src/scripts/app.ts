278f36da5f0c76fa1ce2583bbd4ba4e8e6f7c3e6
markkennerley <19432098+markkennerley@users.noreply.github.com>
2021-03-28 16:31:57 +0100

    Initial Design pt 3 of 3

---
+++ src/scripts/app.ts
@@ -0,0 +1,21 @@
+/*
+
+$("btn-submit").on( "click", function() {
+    var request = $.ajax({
+        url: "/customer/account/resetPassword",
+        method: "POST",
+        data: { //get email from input emailAddress},
+        dataType: "json"
+      });
+       
+      request.done(function( msg ) {
+        //Add a success class to the password-feedback div and populate content
+      });
+       
+      request.fail(function( jqXHR, textStatus ) {
+        //Add a error class to the password-feedback div and populate content
+      });
+});
+
+*/
+
